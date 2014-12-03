6608288f4279dcb215d757a01576c569a3f46f31
calvin <calvinkcollins@gmail.com>
2014-12-02 21:26:36 -0500

    setup foundation template

--- blog/views.py
+++ blog/views.py
@@ -2,34 +2,10 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from blog.models import Category, Article
 import calendar, datetime
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 def index(request) :
-  """The news index"""
+  """The blog index"""
 
-  return HttpResponse('django 1.7 on Openshift') 
-   #  archive_dates = Article.objects.dates('date_publish','month', order='DESC')
-   #  categories = Category.objects.all()
-
-   #  page = request.GET.get('page')
-   #  article_queryset = Article.objects.all()
-   #  paginator = Paginator(article_queryset, 5)
-
-   # try:
-   #      articles = paginator.page(page)
-   #  except PageNotAnInteger:
-   #      # If page is not an integer, deliver first page.
-   #      articles = paginator.page(1)
-   #  except EmptyPage:
-   #      # If page is out of range (e.g. 9999), deliver last page of results.
-   #      articles = paginator.page(paginator.num_pages)
-
-    # return render(
-    #     request,
-    #     "blog/article/index.html",
-    #     {
-    #         "articles" : articles,
-    #         "archive_dates" : archive_dates,
-    #         "categories" : categories
-    #     }
-    # )
+  # return HttpResponse('django 1.7 on Openshift') 
+  return render(request, 'blog/base.html')
--- mysite/settings.py
+++ mysite/settings.py
@@ -7,6 +7,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # openshift is our PAAS for now.
 ON_PAAS = 'OPENSHIFT_REPO_DIR' in os.environ
 
+# here() gives us file paths from the root of the system to the directory
+# holding the current file.
+here = lambda * x: os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
+
+PROJECT_ROOT = here("..")
+# root() gives us file paths from the root of the system to whatever
+# folder(s) we pass it starting at the parent directory of the current file.
+root = lambda * x: os.path.join(os.path.abspath(PROJECT_ROOT), *x)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -31,6 +39,13 @@ if ON_PAAS:
 else:
     ALLOWED_HOSTS = []
 
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    '/home/calvin/dev/blog/templates/',
+    root('templates'),
+)
 
 # Application definition
 
